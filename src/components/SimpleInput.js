78ed5b802f9ec0d2f42c75258b12246261144e7e
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-02-07 20:51:52 -0800

    basic access of entered value useing useRef and useState

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,31 @@
+import { useState, useRef } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const name = useRef();
+  const nameChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredName);
+    console.log(name.current.value);
+    // setEnteredName("");
+    name.current.value = "";
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={submitFormHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameChangeHandler}
+          ref={name}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
