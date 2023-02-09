d776b5e6408e65551f9e253a401fbaed7b0d029e
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-02-09 13:07:54 -0800

    use useReducer for maintaining the two states in custom hook use-Input1

--- src/hooks/use-input1.js
+++ src/hooks/use-input1.js
@@ -1,28 +1,57 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialInputState = { value: "", isTouched: false };
+
+const inputStateReducer = (state, action) => {
+  if (action.type === "CHANGE") {
+    return {
+      value: action.value,
+      isTouched: state.isTouched,
+    };
+  }
+  if (action.type === "BLUR") {
+    return {
+      isTouched: true,
+      value: state.value,
+    };
+  }
+  if (action.type === "RESET") {
+    return {
+      value: "",
+      isTouched: false,
+    };
+  }
+  return initialInputState;
+};
 
 const useInput1 = (validateInput) => {
-  const [enteredValue, setEnteredValue] = useState("");
-  const [isTouched, setIsTouched] = useState(false);
+  const [inputState, dispatch] = useReducer(
+    inputStateReducer,
+    initialInputState
+  );
 
-  const isValueValid = validateInput(enteredValue);
-  const hasError = !isValueValid && isTouched;
+  const isValueValid = validateInput(inputState.value);
+  const hasError = !isValueValid && inputState.isTouched;
 
   const valueChangeHandler = (event) => {
-    setEnteredValue(event.target.value);
-    setIsTouched(true);
+    dispatch({ type: "CHANGE", value: event.target.value });
+    // setEnteredValue(event.target.value);
+    // setIsTouched(true);
   };
 
   const blurHandler = (event) => {
-    setIsTouched(true);
+    dispatch({ type: "BLUR" });
+    // setIsTouched(true);
   };
 
   const reset = () => {
-    setEnteredValue("");
-    setIsTouched(false);
+    dispatch({ type: "RESET" });
+    // setEnteredValue("");
+    // setIsTouched(false);
   };
 
   return {
-    enteredValue,
+    enteredValue: inputState.value,
     isValueValid,
     hasError,
     valueChangeHandler,
